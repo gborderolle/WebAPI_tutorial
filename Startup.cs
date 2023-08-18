09cc01be10704c71227f44d24b943263b0a394c9
Gonzalo Borderolle <gborderolle@gmail.com>
2023-08-18 00:37:09 -0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace API_testing3
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Configuración de los Services
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+
+        /// <summary>
+        /// Configuración del Middleware
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
